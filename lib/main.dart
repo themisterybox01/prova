caf60ad3f026531ae274adec26e886288a52ea93
themisterybox01 <themisterybox01@gmail.com>
2023-07-05 17:17:18 +0200

    Cambiato il contatore +3

--- lib/main.dart
+++ lib/main.dart
@@ -66,6 +66,7 @@ class _MyHomePageState extends State<MyHomePage> {
       // called again, and so nothing would appear to happen.
       _counter++;
       _counter++;
+      _counter++;
     });
   }
 
